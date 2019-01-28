35d8ac5d9e778a84912863d10a5f74dcac042ca5
HandSomeGuy <handsomeguy@HandSomeGuyui-MacBook-Pro.local>
2019-01-28 15:38:12 +0900

    first commit

---
+++ GitTest/GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by HandSomeGuy on 2019. 1. 28..
+//  Copyright © 2019년 HandSomeGuy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
